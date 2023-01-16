1c6ed1363880a1ff9abcba910119845482fdc96e
Lucho <bravo.luis.1995@gmail.com>
2023-01-16 10:00:42 -0500

    Empezando a llenar la base de datos y enlazar al php

--- add.php
+++ add.php
@@ -1,22 +1,16 @@
 <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $contact = [
-        "name" => $_POST["name"],
-        "phone_number" => $_POST["phone_number"],
-    ];
 
-    if (file_exists("contacts.json")) {
-        $contacts = json_decode(file_get_contents("contacts.json"), true);
-    } else {
-        $contacts = [];
-    }
+require "database.php";
 
-    $contacts[] = $contact;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $phoneNumber = $_POST["phone_number"];
 
-    file_put_contents("contacts.json", json_encode($contacts));
+    $statement = $conn->prepare("INSERT INTO contacts (name, phone_number) VALUES ('$name', '$phoneNumber')");
+    $statement->execute();
 
     header("Location: index.php");
-    }
+}
 ?>
 
 
